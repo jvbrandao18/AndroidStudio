62c9a8e7058ab8b8dc5931a432e4970d9700128f
Brandao <66882890+jvbrandao18@users.noreply.github.com>
2022-05-08 19:24:48 -0300

    Tela de Login concluida
    Tela de login feita e cadastro também, porém precisa de ajustes

---
+++ main/java/com/example/meuapp/FormLogin.java
@@ -0,0 +1,16 @@
+package com.example.meuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+    }
+}
